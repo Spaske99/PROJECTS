8d5d9b74764386535cc7d1dc5a547d2f96f56637
Spaske99 <nenad.spasic.99@gmail.com>
2022-05-18 17:34:45 +0200

    hw 15-16

--- hw14/Drugi/index.php
+++ hw14/Drugi/index.php
@@ -7,14 +7,14 @@ function gorivo($ukupnoGorivo = 0, $potrposnjaPoKamionu = 1, $ostatakGorivaFlag 
     $brojKamionaKojiMoguDaPredjuRutu = $potrebnoGorivo / $potrposnjaPoKamionu;
 
     if($ostatakGorivaFlag == true){
-        return "Rutu mogu preci " . $brojKamionaKojiMoguDaPredjuRutu . " kamiona.";
+        return "Rutu mogu preci " . floor($brojKamionaKojiMoguDaPredjuRutu) . " kamiona.";
     }
     else{
         return "Ostalo je " . $ostatakGoriva . " litra goriva.";
     }
 }
 
-echo gorivo(106, 20);
+echo gorivo(-113,20);
 
 //NACIN 2
 // function gorivo($ukupnoGorivo, $potrposnjaPoKamionu, $ostatakGorivaFlag = "0"){
